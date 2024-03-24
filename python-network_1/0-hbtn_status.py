fa3e1bfe8ee237a378fd76deca88ec232b6d206a
denismitali17 <d.mitali1@alustudent.com>
2024-03-24 07:28:54 -0700

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that fetches https://alu-intranet.hbtn.io/status
-- using the urllib package.
-"""
-import urllib.request
+"""Python script that fetches."""
 
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+import requests
+if __name__ == "__main__":
+    requ = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(requ.text)))
+    print("\t- content: {}".format(requ.text))
